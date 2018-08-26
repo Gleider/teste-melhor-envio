e5eccb54781d9f9f61eea5a85bd4a73635bfcd9b
gleider <gmdcampos@inf.ufpel.edu.br>
2018-08-26 14:40:48 -0300

    adicionado configuração com o mongo

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
 const app = express()
 const router = express.Router()
+
+mongoose.connect('mongodb://gleider:gleider123@ds133262.mlab.com:33262/melhor_envio',
+  { useNewUrlParser: true})
 //Rotas
 const index = require('./routes/index')
-const personRoute = require('./routes/personRoute')
+const personRoute = require('./routes/produtoRoute')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
 app.use('/', index)
 app.use('/persons', personRoute)
+
 module.exports = app

d3e6f53d556222a17602e78bde964a73a37c2ff1
gleider <gmdcampos@inf.ufpel.edu.br>
2018-08-29 21:43:12 -0300

    criação do controle de projeto

--- src/controllers/produtoController.js
+++ src/controllers/produtoController.js
@@ -1,5 +1,26 @@
+const mongoose = require('mongoose')
+const Produto = mongoose.model('Produto')
+
+exports.get = (req, res, next) => {
+  Produto.find({}, 'codigo quantidade volumes')
+    .then(data => {
+      res.status(200).send(data)
+    }).catch(erro => {
+      res.status(400).send({ data : erro})
+    })
+}
+
 exports.post = (req, res, next) => {
-  res.status(201).send('Requisição recebida com sucesso!')
+  let produto = new Produto(req.body)
+  console.log(produto)
+  produto
+    .save()
+    .then(data => {
+      res.status(201).send({ message : "Produto cadastrado com sucesso"})
+    }).catch(erro => {
+      res.status(400).send({ message: "Falha ao cadastrar", data : erro})
+    })
+
 }
 
 exports.put = (req, res, next) => {

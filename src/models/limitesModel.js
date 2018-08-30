814b83c1335222b04bc47be692f4566610f49a52
gleider <gmdcampos@inf.ufpel.edu.br>
2018-08-29 21:45:11 -0300

    criação do modelo para os limites de entrega

---
+++ src/models/limitesModel.js
@@ -0,0 +1,32 @@
+exports.correios = {
+  pesoMin: 0,
+  pesoMax: 30000,
+  alturaMin: 2,
+  alturaMax: 105,
+  larguraMin: 11,
+  larguraMax: 105,
+  comprimentoMin: 16,
+  comprimentoMax: 105
+}
+
+exports.jadlog = {
+  pesoMin: 0,
+  pesoMax: 150000,
+  alturaMin: 1,
+  alturaMax: 100,
+  larguraMin: 1,
+  larguraMax: 105,
+  comprimentoMin: 1,
+  comprimentoMax: 181
+}
+
+exports.viaBrasil = {
+  pesoMin: 0,
+  pesoMax: 200,
+  alturaMin: 1,
+  alturaMax: 200,
+  larguraMin: 1,
+  larguraMax: 200,
+  comprimentoMin: 1,
+  comprimentoMax: 240
+}

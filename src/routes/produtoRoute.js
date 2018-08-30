4bbeba3479fa5911dbc9ccf61713128fc89fcbdd
gleider <gmdcampos@inf.ufpel.edu.br>
2018-08-29 21:47:33 -0300

    modificação nas rotas de produto

--- src/routes/produtoRoute.js
+++ src/routes/produtoRoute.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/produtoController')
 
+router.get('/', controller.get)
 router.post('/', controller.post)
 router.put('/:id', controller.put)
-router.delete('/:id', controller.delete)
+router.delete('/', controller.delete)
 
 module.exports = router

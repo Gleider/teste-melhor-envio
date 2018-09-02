afcfe1fd76f926e630183ec60a761640b8ee3f8b
gleider <gmdcampos@inf.ufpel.edu.br>
2018-09-02 19:30:34 -0300

    refatorando

--- src/app.js
+++ src/app.js
@@ -3,14 +3,10 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
 const app = express()
-const router = express.Router()
 
-mongoose.connect('mongodb://gleider:gleider123@ds133262.mlab.com:33262/melhor_envio',
-  { useNewUrlParser: true})
-
+mongoose.connect('mongodb://gleider:gleider123@ds133262.mlab.com:33262/melhor_envio', { useNewUrlParser: true})
 require('./models/produtoModel')
-//Rotas
-const index = require('./routes/index')
+
 const produtoRoute = require('./routes/produtoRoute')
 const comprarRoute = require('./routes/comprarRoute')
 
@@ -19,7 +15,6 @@ app.use(bodyParser.urlencoded({
   extended: false
 }))
 
-app.use('/', index)
 app.use('/produtos', produtoRoute)
 app.use('/comprar', comprarRoute)
 

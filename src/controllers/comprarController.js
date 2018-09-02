f70a7575f7fab4884d732c585351310c122e9c6a
gleider <gmdcampos@inf.ufpel.edu.br>
2018-09-02 16:54:08 -0300

    refatorando controller para possibilitar outras empresas

--- src/controllers/comprarController.js
+++ src/controllers/comprarController.js
@@ -3,9 +3,22 @@ const Produto = mongoose.model('Produto')
 const empacotar = require('../modules/empacotar')
 const entrega = require('../modules/finalizarEntrega')
 
+function envioEmpresa(produto, empresa) {
+  const pacotes = empacotar.entrada(produto, empresa)
+  if(pacotes.erro){
+    return pacotes
+
+  } else {
+    const pacoteFinal = entrega.finalizar(pacotes)
+    return pacoteFinal
+  }
+}
+
 exports.post = (req, res, next) => {
   let produto = new Produto(req.body)
-  const pacotes = empacotar.entrada(produto)
-  const pacoteFinal = entrega.finalizar(pacotes)
-  res.status(200).send(pacoteFinal)
+  let envio = []
+  envio.push(envioEmpresa(produto, 'correios'))
+  envio.push(envioEmpresa(produto, 'jadlog'))
+  envio.push(envioEmpresa(produto, 'viaBrasil'))
+  res.status(200).send(envio)
 }
